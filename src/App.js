80ebf2fbb2a6168eb4a650f83956a9715be2d856
blakelinkd <blakelinkd@gmail.com>
2021-09-11 09:18:48 -0500

    Add and Remove tasks
    You can now add and remove tasks from the list.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Todo from "./components/Todo";
 import { useState } from "react";
+import "./App.css";
 function App() {
   const tasks = [
     {
@@ -17,7 +18,25 @@ function App() {
   ];
 
   const [todoItems, setTodoItems] = useState(tasks);
+  const onSubmit = (e) => {
+    // tasks = {
+    //   ...tasks,
+    //   id: tasks.length + 1,
+    //   title: e.text,
+    //   description: "smell the farts",
+    //   incomplete: true,
+    // };
 
+    // setTodoItems(...tasks, {
+    //   id: tasks.length + 1,
+    //   title: e.text,
+    //   description: e.text,
+    //   incpomplete: true,
+    // });
+
+    console.debug(tasks);
+  };
+  const [text, setText] = useState("");
   const [isDeleted, setIsDeleted] = useState(false);
   const handleDeleteTask = (id) => {
     console.log("delete clicked" + " on " + id);
@@ -41,20 +60,53 @@ function App() {
           )}
         </ul>
 
-        <div className="input-group mb-3 fixed-bottom w-75 mx-auto shadow p-3 mb-5 bg-body rounded">
-          <div className="input-group-prepend">
-            <button className="btn btn-info" type="button">
-              New Task
-            </button>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            setTodoItems([
+              ...todoItems,
+              {
+                id: tasks.length + 1,
+                title: text,
+                description: text,
+                incomplete: true,
+              },
+            ]);
+            console.log(todoItems);
+          }}
+        >
+          <div className="mt-5 form-control input-group mb-3 fixed-middle w-25 mx-auto shadow p-3 mb-5 bg-body rounded">
+            <div className="input-group-prepend">
+              <button
+                type="button"
+                className="btn new-task--button"
+                onClick={(e) => {
+                  setTodoItems([
+                    ...todoItems,
+                    {
+                      id: tasks.length + 1,
+                      title: text,
+                      description: text,
+                      incomplete: true,
+                    },
+                  ]);
+                  console.log(todoItems);
+                }}
+              >
+                New Task
+              </button>
+            </div>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Today I need to..."
+              aria-label=""
+              aria-describedby="basic-addon1"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+            />
           </div>
-          <input
-            type="text"
-            className="form-control"
-            placeholder=""
-            aria-label=""
-            aria-describedby="basic-addon1"
-          />
-        </div>
+        </form>
       </div>
     </div>
   );
